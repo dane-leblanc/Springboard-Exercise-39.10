32917685309e8c2560ef83061d7d1f20d6fd8b71
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-05-26 21:32:56 -0600

    Exercise Complete

--- src/helpers.js
+++ src/helpers.js
@@ -13,11 +13,17 @@ function formatCard(data) {
   };
 }
 
-// function formatPokemon(data) {
-//   return {
-//     id: uuid();
-//     front: 
-//   }
-// }
+function formatPokemon(data) {
+  return {
+    id: uuid(),
+    front: data.sprites.front_default,
+    back: data.sprites.back_default,
+    name: data.name,
+    stats: data.stats.map((stat) => ({
+      value: stat.base_stat,
+      name: stat.stat.name,
+    })),
+  };
+}
 
-export { choice, formatCard };
+export { choice, formatCard, formatPokemon };
